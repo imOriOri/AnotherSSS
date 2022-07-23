af16561014bc3c4de798653d3d81893f2af8226f
Perspective05 <wonsgaddy92@gmail.com>
2022-07-24 02:12:32 +0900

    Update MapManager.cs

--- Assets/Scripts/Map/MapManager.cs
+++ Assets/Scripts/Map/MapManager.cs
@@ -2,17 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MapManager : MonoBehaviour
+public static class MapManager
 {
-    // Start is called before the first frame update
-    void Start()
+    public static HashSet<Vector2Int> Randomwalk(Vector2Int startPosition, int walkLenght) 
     {
-        
-    }
+        HashSet<Vector2Int> path = new HashSet<Vector2Int>();
 
-    // Update is called once per frame
-    void Update()
+        path.Add(startPosition);
+        var previousPosition = startPosition;
+
+        for (int i = 0; i < walkLenght; i++) 
+        {
+            var newPosition = previousPosition + Direction2D.GetRandomCardianlDirection();
+            path.Add(newPosition);
+            previousPosition = newPosition;
+        }
+        return path;
+    }
+}
+
+public static class Direction2D
+{
+    public static List<Vector2Int> cardinalDirectionList = new List<Vector2Int>
     {
-        
+        new Vector2Int(0,1), //Up
+        new Vector2Int(1,0), //Right
+        new Vector2Int(0, -1), //Down
+        new Vector2Int(-1, 0) //Left
+    };
+
+    public static Vector2Int GetRandomCardianlDirection() 
+    {
+        return cardinalDirectionList[Random.Range(0, cardinalDirectionList.Count)];
     }
 }
