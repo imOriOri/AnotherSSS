db9e3dfb8389693dd38862006123e478ceb75872
HeaoleumDal <88084889+HeaoleumDal@users.noreply.github.com>
2022-07-29 17:20:45 +0900

    Player Run & Double Jump
    플레이어 달리기 & 더블 점프 구현
    PlayerMove 코드 정리
    ※대쉬가 그냥 달리긴지 다른 건지 몰라서 그냥 변수만 추가

--- Assets/Scripts/Player/PlayerMove.cs
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -4,36 +4,43 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    public float moveSpeed;
-    public float jumpSpeed;
+    [Header("???? ?? ??")]
+    [SerializeField]
+    private float walkSpeed;
+    [SerializeField]
+    private float runSpeed;
+    [SerializeField]
+    private float jumpSpeed;
+        
+    private float applySpeed;
+
+    [Header("???? ??? ??")]
+    public bool isDoubleJumping = false;
+    public bool isDashing = false;
+
+    int jumpCount = 2;
     bool isJumping = false;
+    bool isRunning = false;
+
     private Rigidbody2D rigid;
 
-    // Start is called before the first frame update
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        jumpCount = 0;
+        applySpeed = walkSpeed;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonUp("Horizontal"))
-        {
-            rigid.velocity = new Vector2(rigid.velocity.normalized.x * 0.2f, rigid.velocity.y);
-        }
-
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            if (!isJumping)
-            {
-                isJumping = true;
-                rigid.AddForce(Vector3.up * jumpSpeed * 100f);
-            }
-        }
+        Moving();
+        Jumping();
+        Running();
+        Walking();
     }
 
-    private void FixedUpdate()
+    //???? ??
+    private void Moving()
     {
         float h = Input.GetAxisRaw("Horizontal");
 
@@ -46,15 +53,63 @@ public class PlayerMove : MonoBehaviour
             transform.localScale = new Vector3(1, 1, 1);
         }
 
-        if (rigid.velocity.x > moveSpeed)
+        if (rigid.velocity.x > applySpeed)
         {
-            rigid.velocity = new Vector2(moveSpeed, rigid.velocity.y);
+            rigid.velocity = new Vector2(applySpeed, rigid.velocity.y);
         }
-        else if (rigid.velocity.x < -moveSpeed)
+        else if (rigid.velocity.x < -applySpeed)
         {
-            rigid.velocity = new Vector2(-moveSpeed, rigid.velocity.y);
+            rigid.velocity = new Vector2(-applySpeed, rigid.velocity.y);
         }
+
         rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
+
+        if (Input.GetButtonUp("Horizontal"))
+        {
+            rigid.velocity = new Vector2(rigid.velocity.normalized.x * 0.2f, rigid.velocity.y);
+        }
+    }
+
+    //???? ??
+    private void Jumping()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (!isJumping)
+            {
+                if(jumpCount > 0)
+                {
+                    if (isDoubleJumping)
+                    {
+                        rigid.AddForce(Vector3.up * jumpSpeed * 100f);
+                        jumpCount--;
+                    }
+                    else if (!isDoubleJumping)
+                    {
+                        isJumping = true;
+                        rigid.AddForce(Vector3.up * jumpSpeed * 100f);
+                    }
+                }
+            }
+        }
+    }
+
+    //???? ???
+    private void Running()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            applySpeed = runSpeed;
+        }
+    }
+
+    //???? ??
+    private void Walking()
+    {
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            applySpeed = walkSpeed;
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -62,6 +117,7 @@ public class PlayerMove : MonoBehaviour
         if (collision.gameObject.name == "Platform")
         {
             isJumping = false;
+            jumpCount = 2; 
         }
     }
 }
