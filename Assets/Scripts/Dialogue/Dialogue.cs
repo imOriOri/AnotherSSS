1111f006e6a71de6d43861af960e7bd6e49c612e
Perspective05 <wonsgaddy92@gmail.com>
2022-07-30 12:52:53 +0900

    TMPro

---
+++ Assets/Scripts/Dialogue/Dialogue.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dialogue : MonoBehaviour
+{
+    public GameObject window;
+    public GameObject indicator;
+
+    public List<string> dialouges;
+
+    private int index;
+    private int charIndex;
+
+    public void StartDialogue()
+    {
+
+    }
+    public void EnDialogue() 
+    {
+    }
+    IEnumerator Writing() 
+    {
+        yield return null;
+    }
+}
